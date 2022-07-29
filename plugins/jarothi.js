2c16f4b2913537dbc62d626aa4f8726d962282aa
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-07-30 00:27:33 +0700

    Update jarothi.js

--- plugins/jarothi.js
+++ plugins/jarothi.js
@@ -4,11 +4,11 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn }) => {
 
-let hallo = fs.readFileSync('./mp3/WhatsApp-Audio-2021-03-02-at-20.52.32-_2_.opus') 
+let hallo = fs.readFileSync('./mp3/WhatsApp-Ptt-2021-07-14-at-18.12.33.opus') 
 
 conn.sendFile(m.chat, hallo, '', '', m, true)
 
-//conn.sendMessage(m.chat, helloaine, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
+//conn.sendMessage(m.chat, halojarot, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 
 // await conn.sendMessage(m.chat, { audio: { url: helloaine }, mimetype: 'audio/mp4'}, m)
 
