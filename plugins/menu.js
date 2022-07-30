44d079ba9ea4b0f3e616b96707a035bb7709a4c7
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-07-30 15:28:22 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -200,7 +200,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
         title: '𝐥𝐢𝐬𝐭 𝐦𝐞𝐧𝐮 𝐀𝐥𝐩𝐢 𝐛𝐨𝐭',
         rows: [
           { title: 'Allmenu', rowId: `${_p}? all` },
-          { title: 'info alpi', rowId: `${_p}? infoalpi` },
+       
           { title: 'info jarot', rowId: `${_p}? infojarot` },
           { title: 'Game', rowId: `${_p}? game` },
           { title: 'XP', rowId: `${_p}? xp` },
